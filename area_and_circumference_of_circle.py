7c34b041bb0e5a86ff1097cd76ee1a08d0f02ee7
Ubuntu <ubuntu@ip-172-31-11-30.ec2.internal>
2020-11-24 01:07:54 +0000

    added area_and_circumference_of_circle.py

---
+++ area_and_circumference_of_circle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# Created by Marlon Poddalgoda
+# Created on November 2020
+# This program calculates the area and circumference of a circle
+#     with a radius of 15mm
+
+import math
+
+
+def main():
+    # this function calculates/prints the area & circumference of the circle
+    print("If a circle has a radius of 15 mm:")
+    print("")
+    print("Then...")
+    print("")
+    print("Area is {} mm²".format(math.pi*15**2))
+    print("Circumference is {} mm".format(2*math.pi*15))
+
+
+if __name__ == "__main__":
+    main()
